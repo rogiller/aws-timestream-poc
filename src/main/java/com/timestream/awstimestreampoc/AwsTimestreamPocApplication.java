3cfd3aae7cddb259d13f9bd846cce1123c32f714
Roger Diller <roger@flexrentalsolutions.com>
2022-05-16 16:50:00 -0400

    initial commit

---
+++ src/main/java/com/timestream/awstimestreampoc/AwsTimestreamPocApplication.java
@@ -0,0 +1,13 @@
+package com.timestream.awstimestreampoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTimestreamPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTimestreamPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timestream/awstimestreampoc/AwsTimestreamPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timestream.awstimestreampoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTimestreamPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
