31749867486c1533b7518c941f9e232eca55c5c6
Roger Diller <roger@flexrentalsolutions.com>
2022-05-16 17:14:43 -0400

    post some data

--- src/test/java/com/timestream/awstimestreampoc/AWSTimeStreamTest.java
+++ src/test/java/com/timestream/awstimestreampoc/AWSTimeStreamTest.java
@@ -3,10 +3,11 @@ package com.timestream.awstimestreampoc;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.services.timestreamwrite.AmazonTimestreamWrite;
 import com.amazonaws.services.timestreamwrite.AmazonTimestreamWriteClientBuilder;
-import com.amazonaws.services.timestreamwrite.model.Database;
-import com.amazonaws.services.timestreamwrite.model.ListDatabasesRequest;
-import com.amazonaws.services.timestreamwrite.model.ListDatabasesResult;
+import com.amazonaws.services.timestreamwrite.model.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AWSTimeStreamTest {
 
@@ -33,6 +34,47 @@ public class AWSTimeStreamTest {
 
         for (Database db : result.getDatabases()) {
             System.out.println(db.getDatabaseName());
+        }
+
+        // Specify repeated values for all records
+        List<Record> records = new ArrayList<>();
+        final long time = System.currentTimeMillis();
+
+        List<Dimension> dimensions = new ArrayList<>();
+        final Dimension region = new Dimension().withName("region").withValue("us-east-2");
+        final Dimension hostname = new Dimension().withName("hostname").withValue("roger-localhost");
+        final Dimension tenant = new Dimension().withName("tenant").withValue("roger");
+
+        dimensions.add(region);
+        dimensions.add(hostname);
+        dimensions.add(tenant);
+
+        Record transactionTime = new Record()
+                .withDimensions(dimensions)
+                .withMeasureName("transaction_time")
+                .withMeasureValue(String.valueOf(Math.random() * 100))
+                .withMeasureValueType(MeasureValueType.DOUBLE)
+                .withTime(String.valueOf(time));
+
+        records.add(transactionTime);
+
+        WriteRecordsRequest writeRecordsRequest = new WriteRecordsRequest()
+                .withDatabaseName("roger-test")
+                .withTableName("roger-table1")
+                .withRecords(records);
+
+        try {
+            WriteRecordsResult writeRecordsResult = amazonTimestreamWrite.writeRecords(writeRecordsRequest);
+            System.out.println("WriteRecords Status: " + writeRecordsResult.getSdkHttpMetadata().getHttpStatusCode());
+        } catch (RejectedRecordsException e) {
+            System.out.println("RejectedRecords: " + e);
+            for (RejectedRecord rejectedRecord : e.getRejectedRecords()) {
+                System.out.println("Rejected Index " + rejectedRecord.getRecordIndex() + ": "
+                        + rejectedRecord.getReason());
+            }
+            System.out.println("Other records were written successfully. ");
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
         }
 
 
